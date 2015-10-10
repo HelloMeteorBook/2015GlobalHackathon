489d79235236175b3c6e3a7163c2db6dbc14d594
Ben Cohen <ben@bencohen.net>
2015-10-10 15:07:49 -0400

    Line item price updating

--- simple-invoice/client/lineItem.js
+++ simple-invoice/client/lineItem.js
@@ -1,17 +1,28 @@
 Template.lineItem.onCreated(function() {
   // create line item price and quantity
-  
+  this.price = new ReactiveVar(0);
+  this.quantity = new ReactiveVar(0);
 });
 
 Template.lineItem.helpers({ 
   lineItemPrice:function() {
-    return 10;
+    var display;
+    var totalPrice = Template.instance().price.get() * Template.instance().quantity.get()
+    if(totalPrice > 0) {
+      display = '$' + totalPrice;
+    } else {
+      display = '$0'
+    }
+    return display;
   }
 }); 
 
 Template.lineItem.events({ 
   // some event that adds the line item to a client side line item collection
-  "click #foo": function(event, template){ 
-     
-  } 
+  "keyup [name=item-price]": function(event, template){ 
+    template.price.set(event.target.value);
+  },
+  "keyup [name=item-quantity]": function(event, template){ 
+    template.quantity.set(event.target.value);
+  }
 }); 
