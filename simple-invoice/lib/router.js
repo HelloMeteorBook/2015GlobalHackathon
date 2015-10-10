fe2ea2a316c2b0f7a453913a1d5891583ed3e62a
Will Vincent <william.s.vincent@gmail.com>
2015-10-10 08:35:22 -0400

    add fastclick meteor package; create router.js file

---
+++ simple-invoice/lib/router.js
@@ -0,0 +1,17 @@
+Router.configure({
+  layoutTemplate: 'layout',
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound'
+});
+
+// STATIC pages
+Router.route('/', {
+  name: 'home',
+  fastRender: true
+});
+
+// STATIC pages
+Router.route('/about', {
+  name: 'aboutPage',
+  fastRender: true
+});
