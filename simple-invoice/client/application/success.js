33eb41e3830aa501c0d1e301b6557e31dc474738
Will Vincent <william.s.vincent@gmail.com>
2015-10-10 23:05:59 -0400

    clipboard.js works now, pretty hacky

--- simple-invoice/client/application/success.js
+++ simple-invoice/client/application/success.js
@@ -1,9 +1,16 @@
-Template.success.helpers({ 
+Template.success.helpers({
   shareableUrl: function() {
     return Template.instance().shareableUrl;
   }
-}); 
+});
 
 Template.success.onCreated(function() {
-  this.shareableUrl = Blaze._globalHelpers.urlFor('pay', {hash: {invoiceId: this.data.invoice._id}});
+  this.shareableUrl = Spacebars.SafeString(Blaze._globalHelpers.urlFor('pay', {hash: {invoiceId: this.data.invoice._id}}));
 });
+
+Template.success.onRendered(function() {
+  var clippy = new Clipboard('.btn-copy-link');
+  clippy.on('success', function(e) {
+    console.info('Action:', e.action);
+  });
+})
